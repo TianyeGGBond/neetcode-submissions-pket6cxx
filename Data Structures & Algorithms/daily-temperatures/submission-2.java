b4b6d525eb7367c36db774bb179f709436410d24
Tianye Dong <tianye35010@gmail.com>
2026-04-15 15:49:42 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        Stack<int[]> prev_temp = new Stack<>();
+        int[] cooler_day = new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++){
+            int[] cur = new int[]{temperatures[i], i};
+            //not empty
+            //cur is warmer
+            while(!prev_temp.isEmpty() && prev_temp.peek()[0] < cur[0]){
+                int[] prev = prev_temp.pop();
+                cooler_day[prev[1]] = i - prev[1]; 
+            }
+            prev_temp.push(cur);
+        }
+
+        return cooler_day;
+    }
+}
