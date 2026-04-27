688267ba88b0a9dc6610a1f7d6a99be883aba613
Tianye Dong <tianye35010@gmail.com>
2026-04-27 14:28:18 -0400

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxBnn = 0;
+        for(int bnn : piles){
+            maxBnn = Math.max(bnn, maxBnn);
+        }
+
+        int tryL = 1, tryR = maxBnn + 1;
+        while(tryL < tryR){
+            int tryM = tryL + (tryR - tryL) /2;
+
+            if(hour(tryM, piles) <= h){
+                tryR = tryM ;
+            }else{
+                tryL = tryM + 1;
+            }
+        }
+
+        return tryL;
+    }
+
+    private int hour(int eatRate, int[] piles){
+        int count = 0;
+        for(int bnn : piles){
+            count += (bnn + eatRate - 1) / eatRate;
+        }
+        return count;
+    }
+}
+// lower bound 最小的 k，使得 hour(k) <= h
+
+// if (mid 已经满足条件) {
+//     r = mid;
+// } else {
+//     l = mid + 1;
+// }
