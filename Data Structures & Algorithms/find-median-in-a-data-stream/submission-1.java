2e21bc8379beefaf3a5ea9727020f054e9a00025
Tianye Dong <tianye35010@gmail.com>
2026-06-01 18:58:42 -0400

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.java
@@ -0,0 +1,24 @@
+class MedianFinder {
+    //larger part minHeap
+    Queue<Integer> large;
+
+    //smaller part maxHeap
+    Queue<Integer> small;
+
+    public MedianFinder() {
+        large = new PriorityQueue<>();
+        small = new PriorityQueue<>((a,b) -> Integer.compare(b,a));
+    }
+    
+    public void addNum(int num) {
+        small.offer(num);
+        large.offer(small.poll());
+        if(large.size() > small.size()) small.offer(large.poll());
+    }
+    
+    public double findMedian() {
+        if(small.size() > large.size()) return small.peek();
+
+        return ((long)small.peek() + large.peek())/2.0;
+    }
+}
