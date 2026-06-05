8345713591a633393d03fb3a4246ecced707b2fb
Tianye Dong <tianye35010@gmail.com>
2026-06-04 22:22:43 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] pair : prerequisites){
+            int pre = pair[1];
+            int cur = pair[0];
+            graph.get(pre).add(cur);
+            indegree[cur]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) queue.offer(i);
+        }
+
+        int count = 0;
+
+        while(!queue.isEmpty()){
+            int course = queue.poll();
+            count++;
+            List<Integer> nexts = graph.get(course);
+            for(int next : nexts){
+                indegree[next]--;
+                if(indegree[next] == 0){
+                    queue.offer(next);
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
