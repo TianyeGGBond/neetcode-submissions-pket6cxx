393db9efdd4cb08a680cf861bca500894375a67b
Tianye Dong <tianye35010@gmail.com>
2026-06-05 23:18:52 -0400

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+
+        for(int i = 1; i<= n;i++){
+            for(String w : wordDict){
+                int wLen = w.length();
+                if(i - wLen >= 0 && s.substring(i-wLen, i).equals(w)){
+                    dp[i] = dp[i] || dp[i - wLen];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
